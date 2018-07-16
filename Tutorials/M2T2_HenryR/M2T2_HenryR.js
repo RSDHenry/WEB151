928580f67cbfd6093cf4e3f0653f8feee38f0e38
Rashad Henry <rsdhenry@tutanota.com>
2018-07-16 10:32:08 -0400

    Add files via upload

---
+++ Tutorials/M2T2_HenryR/M2T2_HenryR.js
@@ -0,0 +1,65 @@
+Section 2 Console
+
+// Question 1
+console.log("Pepperoni Pizza");
+// Question 2
+console.log("Blade Runner");
+
+Section 3 Data Types
+
+// Question 1
+console.log('JavaScript');
+// Question 2
+console.log(33.7);
+// Question 3
+console.log(true);
+// Question 4
+console.log(null);
+
+Section 4 Math Operators
+
+// Question 1
+console.log(3.5 + 31);
+// Question 2
+console.log(2018 - 1969);
+// Question 3
+console.log(65 / 240);
+//Question 4
+console.log(0.2708 * 100);
+
+Section 5 Properties
+
+// Question 1
+console.log("Teaching the world how to code".length);
+
+Section 6 Built-in Methods
+
+// Question 1
+console.log('Codecademy'.toUpperCase());
+// Question 2
+console.log('    Remove whitespace   '.trim());
+
+Section 7 Libraries
+
+// Question 1
+console.log(Math.random() * 100);
+// Question 2
+console.log(Math.floor(Math.random() * 100));
+// Question 3
+console.log(Math.ceil(43.8));
+// Question 4
+console.log(Number.isInteger(2017));
+
+Section 8 Comments
+
+// Question 1
+// Opening line
+
+// Question 2
+/* console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.'); */
+
